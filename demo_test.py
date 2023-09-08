06a40858d88dddcc742bd90d75a99e7ac77875ac
Andrew Tugman <andrewtugman@Andrews-Air.lan>
2023-09-07 23:47:53 -0400

    Add txc lab files

---
+++ demo_test.py
@@ -0,0 +1,21 @@
+import base64
+
+from locust import HttpUser, task
+from random import randint, choice
+
+class User(HttpUser): 
+
+    @task
+    def load(self):
+
+        catalogue = self.client.get("/catalogue").json()
+        category_item = choice(catalogue)
+        item_id = category_item["id"]
+
+        self.client.get("/")
+        self.client.get("/category.html")
+        self.client.get("/detail.html?id={}".format(item_id))
+        self.client.delete("/cart")
+        self.client.post("/cart", json={"id": item_id, "quantity": 1})
+        self.client.get("/basket.html")
+        self.client.post("/orders")
